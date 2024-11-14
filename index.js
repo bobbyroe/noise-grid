4178eaaecfdd81dd95469a642bd53c008035e964
Bobby Roe <bobby.roe@unifyconsulting.com>
2024-11-13 19:09:44 -0800

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.133.1";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js";
-import { ImprovedNoise } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/math/ImprovedNoise.js";
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+import { ImprovedNoise } from "jsm/math/ImprovedNoise.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -8,90 +8,69 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer();
+renderer.toneMapping = THREE.NoToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
-// Orbit Controls
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 0, 0);
-controls.update();
+controls.enableDamping = true;
 
-// initialize points
-const gridSize = 32;
-const gap = 0.16;
-const coords = [];
-const colors = [];
-const points = [];
-let x;
-let y;
-let z = 0;
-let r;
-let g;
-let b;
-let point = {
-  position: {},
-  rate: 0.0,
-};
-for (let i = -gridSize; i < gridSize; i += 1) {
-  for (let j = -gridSize; j < gridSize; j += 1) {
-    x = i * gap;
-    y = j * gap;
-    r = Math.random();
-    g = Math.random();
-    b = Math.random();
-    point = {
-      position: {
-        x,
-        y,
-        z,
-      },
-      color: new THREE.Color(r, g, b),
-    };
-
-    points.push(point);
-    coords.push(x, y, z);
-    colors.push(r, g, b);
-  }
-}
-
-// points
-const geo = new THREE.BufferGeometry();
-geo.setAttribute("position", new THREE.Float32BufferAttribute(coords, 3));
-geo.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
-const mat = new THREE.PointsMaterial({ size: 0.31, vertexColors: true });
-const pointsObj = new THREE.Points(geo, mat);
+const texLoader = new THREE.TextureLoader();
 const Noise = new ImprovedNoise();
 
-function updatePoints(t) {
-  const coords = [];
-  const cols = [];
-  let ns;
+function getPointsObj() {
+  const planeGeo = new THREE.PlaneGeometry(11, 11, 48, 48);
+  const geo = new THREE.BufferGeometry();
+  const mat = new THREE.PointsMaterial({
+    size: 0.2,
+    vertexColors: true,
+    map: texLoader.load("./circle.png"),
+    alphaTest: 0.5,
+    transparent: true,
+  });
+  const pointsObj = new THREE.Points(geo, mat);
+
+  const coords = planeGeo.attributes.position;
+  let colors = [];
+  let col = new THREE.Color();
+  const p = new THREE.Vector3();
   const nScale = 0.5;
   const zPosScale = 1.5;
   const lowColor = new THREE.Color(0.0, 0, 0.8);
   const highColor = new THREE.Color(1.5, 1.5, 1.5);
-  points.forEach((p, i) => {
-    ns = Noise.noise(p.position.x * nScale, p.position.y * nScale, t);
-    p.position.z = ns * zPosScale;
-    p.color.lerpColors(lowColor, highColor, ns * 1.5);
-    let { r, g, b } = p.color;
-    cols.push(r, g, b);
-    let {x, y, z } = p.position;
-    coords.push(x, y, z);
-  });
-  geo.setAttribute("position", new THREE.Float32BufferAttribute(coords, 3));
-  geo.setAttribute("color", new THREE.Float32BufferAttribute(cols, 3));
+  function update(t) {
+    let ns;
+    colors = [];
+    for (let i = 0; i < coords.count; i += 1) {
+      p.fromBufferAttribute(coords, i);
+      ns = Noise.noise(p.x * nScale, p.y * nScale, t);
+      p.z = ns * zPosScale;
+      coords.setXYZ(i, p.x, p.y, p.z);
+      col.lerpColors(lowColor, highColor, ns * 1.5);
+      let { r, g, b } = col;
+      colors.push(r, g, b);
+    }
+    geo.setAttribute("position", coords);
+    geo.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+    coords.needsUpdate = true;
+  }
+  pointsObj.userData = { update };
+  return pointsObj;
 }
-scene.add(pointsObj);
+
+const points = getPointsObj();
+scene.add(points);
 
 const timeMult = 0.0005;
-function animate(timeStep) {
+function animate(t = 0) {
   requestAnimationFrame(animate);
-  updatePoints(timeStep * timeMult);
+  points.userData.update(t * timeMult);
+  controls.update();
   renderer.render(scene, camera);
 }
-// START!
-animate(0);
+animate();
 
 function handleWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
